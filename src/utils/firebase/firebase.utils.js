7f35ec4e1b7fd6109ab0d3fca5c79b2faf08cb4f
Alexander Lavrov <Jiabp.lad@yandex.com>
2023-01-22 13:07:53 +0500

    Add: Google sign in start

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,18 @@
+import {initializeApp} from "firebase/app";
+import {
+    getAuth,
+    signInWithPopup,
+    signInWithRedirect,
+    GoogleAuthProvider
+} from "firebase/auth"
+import {firebaseConfig} from "./firebase.config"
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
